97c7d570a99978fbcd426ed467732276caf90e67
bhitt <bhitt@student.rcc.edu>
2015-09-10 10:43:50 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Homework/GaddisReview/main.cpp
+++ Homework/GaddisReview/main.cpp
@@ -8,6 +8,7 @@
 //System Libraries
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 //User Libraries
@@ -37,15 +38,16 @@ int main(int argc, char** argv) {
         case '1':{
             cout<<"*****************************"<<endl;
             cout<<"What is the month?"<<endl;
-            string month;
-            cin>>month;
+            string month;//string variable
+            cin>>month;//user input
             cout<<"What year is it?"<<endl;
-            int year;
-            cin>>year;
+            int year;//year variable
+            cin>>year;//user input for year
             cout<<"What is the total amount collected? (Sales tax plus sales)"<<endl;
-            float collect,sales;
-            cin>>collect;
-            sales=collect/1.06;
+            float collect,sales;//sales, total amount collected variables
+            cin>>collect;//user input for total amount collected
+            sales=collect/1.06;//sales 
+            //Output the results
             cout<<"Month: "<<month<<endl;
             cout<<"------------"<<endl;
             cout<<"Total Collected:  $"<<collect<<endl;
@@ -59,10 +61,10 @@ int main(int argc, char** argv) {
         case '2':{
             cout<<"*****************************"<<endl;
             cout<<"What is the actual value of the property?"<<endl;
-            float actual,assess,propTax;
-            cin>>actual;
-            assess=actual*0.60;
-            propTax=assess*0.0064;
+            float actual,assess,propTax;//variables
+            cin>>actual;//input for actual value
+            assess=actual*0.60;//assessment
+            propTax=assess*0.0064;//property taxes based on assessment
             cout<<"The assessment value is: $"<<assess<<endl;
             cout<<"The property tax is    : $"<<propTax<<endl;
             cout<<"*****************************"<<endl;
@@ -70,35 +72,36 @@ int main(int argc, char** argv) {
             }
         case '3':{
             cout<<"*****************************"<<endl;
-            int units=0;
-            float cost=99;
-            do{
+            int units=0;//units variable
+            float cost=99;//cost per unit
+            do{//check for positive number
             cout<<"What is the number of units sold?"<<endl;
-            cin>>units;
+            cin>>units;//user input for units sold
             }while(units<=0);
-            if(units<10)cost=cost*units;
-            else if(units<19)cost=cost*units-((cost*units)*0.20);
-            else if(units<49)cost=cost*units-((cost*units)*0.30);
-            else if(units<99)cost=cost*units-((cost*units)*0.40);
-            else cost=cost*units-((cost*units)*0.5);
+            if(units<10)cost=cost*units;//less than 10 units,no discount
+            else if(units<19)cost=cost*units-((cost*units)*0.20);//20 percent discount
+            else if(units<49)cost=cost*units-((cost*units)*0.30);//30 % discount
+            else if(units<99)cost=cost*units-((cost*units)*0.40);//40 % discount
+            else cost=cost*units-((cost*units)*0.5);//50 % discount
             cout<<"Your total cost is: $"<<cost<<endl;
             cout<<"*****************************"<<endl;
                 break;
             }
         case '4':{
             cout<<"*****************************"<<endl;
-            int start=0;
-            do{
+            int start=0;//variable for beginning number of organisms
+            do{//check for at least 2 organisms to start with
             cout<<"What is the starting number of organisms?"<<endl;
-            cin>>start;
+            cin>>start;//input for starting organisms
             }while(start<2);
             cout<<"What is the daily percentage increase? (ex:60 for 60%)"<<endl;
-            float incr;
-            cin>>incr;
-            incr*=0.010;
+            float incr;//daily increase
+            cin>>incr;//input for increase
+            incr*=0.010;//turn into a percentage
             cout<<"How many days would you like this to loop?"<<endl;
             int loop=0,day=1;
             cin>>loop;
+            //loop for each day
             for(loop;loop>0;loop--){
                 cout<<"Day ("<<day<<"): "<<start<<endl;
                 day++;
@@ -109,7 +112,8 @@ int main(int argc, char** argv) {
             }
         case '5':{
             cout<<"*****************************"<<endl;
-            float fare=0,cel=0;
+            float fare=0,cel=0;//variables
+            //loop for each degree conversion
             for(int i=0;i<20;i++){
                 cel=celsius(fare);
                 cout<<"F: "<<fare<<" | C: "<<cel<<endl;
@@ -120,10 +124,18 @@ int main(int argc, char** argv) {
             }
         case '6':{
             cout<<"*****************************"<<endl;
+            //declare variables
+            //const int SIZE=20;
+            //int array[SIZE]={};
+            //fill array
             string name;
             cout<<"What is the file name?"<<endl;
             cin>>name;
-            
+            //ofstream outFile;
+            //ifstream inFile;
+            //inFile.open("name.txt");
+            //close the file
+            //outFile.close();
             cout<<"*****************************"<<endl;
                 break;
             }
