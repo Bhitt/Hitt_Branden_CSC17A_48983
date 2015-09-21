4734ee89d6c1d47be0f4cef996b74f4ef33e0e2c
bhitt <bhitt@student.rcc.edu>
2015-09-21 12:31:34 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Homework/Mode/main.cpp
+++ Homework/Mode/main.cpp
@@ -16,30 +16,36 @@ using namespace std;
 //Function Prototypes
 void filAray(int [],int);//fill
 void markSrt(int [], int);//sort
-void cpyAray(int [], int);//copy
 void prntAry(int [],int,int);//print
 int *modeFil(int [],int);//find mode
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE=19;
+    const int SIZE=99;
     int array[SIZE]={};//declare original array
     int copy[SIZE]={};//declare copy of array
     //fill the array
     filAray(array,SIZE);
     //print the original array
+    cout<<"Your array:"<<endl;
     prntAry(array,SIZE,10);
+    cout<<endl;
     //copy the array
     for(int i=0;i<SIZE;i++){
         copy[i]=array[i];
     }
     //sort the array
     markSrt(copy,SIZE);
-    //find the dynamic mode array
+    //find the mode and put in a dynamic array
     int *mode=modeFil(copy,SIZE);
+    //Output mode
+    cout<<endl;
+    cout<<"The Number of Modes is: "<<mode[0]<<endl;
+    cout<<"The Frequency of the Mode(s) is: "<<mode[1]<<endl;
     
-    //Output dynamic array
-    
+    for(int i=2;i<(mode[0]+2);i++){
+        cout<<"Item "<<i-1<<": "<<mode[i]<<endl;
+    }
     //De-allocate memory
     delete []mode;
     //Exit stage right
@@ -74,17 +80,45 @@ void markSrt(int a[],int n){
 //find the mode and freq
 int *modeFil(int a[],int n){
     //declare variables
-    int count=0,temp=0;
+    int count=1,freq=1,nOfM=0;
     int m=0;//mode
-    //loop for mode
+    //loop for freq and number of modes
     for(int i=0;i<n;i++){
         if(a[i]==a[i+1]){
             count++;
         }else{
-            if(count>temp){
-               temp=count; 
+            if(count>freq){
+               freq=count;
+               nOfM=1;
+            }else if(count==freq){
+               nOfM++; 
+            }else{
+                //do nothing
             }
-            count=0;
+            count=1;
         }
     }
+    //allocate memory
+    int *b=new int[m];
+    int temp=2;
+    //loop for modes
+    if(freq<=1){//no modes
+        b[0]=0;
+        b[1]=1;
+    }else{
+        //enter the data
+        b[0]=nOfM;//number of modes
+        b[1]=freq;//frequency of the mode
+        //add the modes
+        for(int i=0;i<n;i++){
+            if(a[i]==a[i+(freq-1)]){
+                b[temp]=a[i];
+                temp++;
+            }else{
+                //do nothing
+            }
+        }
+        return b;
+    }
+    
 }
