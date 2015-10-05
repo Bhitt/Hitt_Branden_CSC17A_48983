70a2d5f380084fcabbb604e44774b8978f0332e7
bhitt <bhitt@student.rcc.edu>
2015-10-05 00:13:17 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

---
+++ Hmwk/Assignment 3/Gaddis_Chap12_Prob8/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Branden Hitt
+ * Created on Oct 4th, 2015, 10:30 PM
+ *      Purpose: read in a binary file
+ */
+
+//System Libraries
+#include <iostream>//I/O standard
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+void aryToFl(string,int *,int);
+void flToAry(string,int *,int);
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+   
+    //Prompt the user for inputs
+    
+    //Output 
+    
+    return 0;
+}
