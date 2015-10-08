4bcf8364beb77b0413f059b8fd2013840683a150
bhitt <bhitt@student.rcc.edu>
2015-10-07 21:10:22 -0700

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

---
+++ Hmwk/Assignment 4/Gaddis_Chap11_Prob1/MovieData.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   MovieData.h
+ * Author: Administrator
+ *
+ * Created on October 7, 2015, 8:30 PM
+ */
+
+#ifndef MOVIEDATA_H
+#define	MOVIEDATA_H
+
+struct MovieData{
+    string title;
+    string direct;
+    unsigned short rlsYear;
+    unsigned short runTime;
+};
+
+#endif	/* MOVIEDATA_H */
+
