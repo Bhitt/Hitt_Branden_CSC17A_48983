ed83a0c47354b9c37f5167d430cbe4b8aad9b603
bhitt <bhitt@student.rcc.edu>
2015-11-23 21:21:59 -0800

    Signed-off-by: bhitt <bhitt@student.rcc.edu>

--- Hmwk/Assignment 6/Gaddis_Chap14_Prob1/Numbers.cpp
+++ Hmwk/Assignment 6/Gaddis_Chap14_Prob1/Numbers.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   card.cpp
  * Author: Branden Hitt
  * Created on November 8th, 2015, 8:29 PM
@@ -9,23 +9,30 @@ using namespace std;
 //User Library for the Specification
 #include "Numbers.h"
 
+string Numbers::less[20]={"zero","one","two","three","four","five","six","seven",
+                   "eight","nine","ten","eleven","twelve","thirteen",
+                   "fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
+string Numbers::more[8] = {"twenty-","thirty-","forty-","fifty-","sixty-",
+                                   "seventy-","eighty-","ninety-"};
+string Numbers::hundred = "hundred";
+string Numbers::thousand = "thousand";
+
 Numbers::Numbers(int n){
     num=n;
 }
 void Numbers::print(){
     //output the thousand value
-//    if(num>999){
-//        cout<<less[num/1000]<<" "<<thousand;
-//    }
-//    //output the hundred value
-//    if((num%1000)>99){
-//        cout<<less[(num%1000)/100]<<" "<<hundred<<" and ";
-//    }
-//    //output the remaining total
-//    if((num%100)>19){
-//        cout<<more[((num%100)/10)-1]<<less[(num%10)+1];
-//    }else{
-//        cout<<less[(num%100)];
-//    }
-    cout<<num<<endl;
+    if(num>999){
+        cout<<less[num/1000]<<" "<<thousand<<" ";
+    }
+    //output the hundred value
+    if((num%1000)>99){
+        cout<<less[(num%1000)/100]<<" "<<hundred<<" and ";
+    }
+    //output the remaining total
+    if((num%100)>19){
+        cout<<more[((num%100)/10)-2]<<less[(num%10)];
+    }else{
+        cout<<less[(num%100)];
+    }
 }
--- Hmwk/Assignment 6/Gaddis_Chap14_Prob1/Numbers.h
+++ Hmwk/Assignment 6/Gaddis_Chap14_Prob1/Numbers.h
@@ -14,22 +14,14 @@ using namespace std;
 class Numbers{
     private:
         int num;
-//        static string less[20];
-//        static string more[8];
+        static string less[20];
+        static string more[8];
         static string hundred;
         static string thousand;
     public:
         Numbers(int);       //Constructor
         void print();       //Display
 };
-
-//string Numbers::less[20]={"zero","one","two","three","four","five","six","seven",
-//                   "eight","nine","ten","eleven","twelve","thirteen",
-//                   "fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-//string Numbers::more[8] = {"twenty-","thirty-","forty-","fifty-","sixty-",
-//                                   "seventy-","eighty-","ninety-"};
-//string Numbers::hundred = "hundred";
-//string Numbers::thousand = "thousand";
 
 #endif	/* NUMBERS_H */
 
